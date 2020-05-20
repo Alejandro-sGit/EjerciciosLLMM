91303dfe607c4bac825fa3638f608da4a5ede92f
Alejandro-sGit <56017946+Alejandro-sGit@users.noreply.github.com>
2020-05-20 14:19:27 +0200

    Add files via upload

---
+++ MyJS.js
@@ -0,0 +1,8 @@
+
+function myFunction(){
+	document.getElementById("demo").innerHTML= "paragraph changed."
+}
+function clear_paragraph(){
+	document.getElementById("demo").style.display="none";
+}
+
